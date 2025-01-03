41b9779cda55c8eeaf47a7719eac98ba9ee9bed4
AmanWasti9 <wastiaman123@gmail.com>
2025-01-04 01:54:35 +0500

    new

--- db.js
+++ db.js
@@ -3,9 +3,9 @@ const mysql = require("mysql2");
 // Create a connection pool
 const db = mysql.createPool({
   host: "sql12.freesqldatabase.com",
-  user: "sql12755550",
-  password: "BhYGlebns3",
-  database: "sql12755550",
+  user: "sql12755553",
+  password: "QlqWSmTqk3",
+  database: "sql12755553",
   connectionLimit: 10,
   port: 3306,
 });
